9884347bbc33f377a33fd74e3e728cdfad97daab
AlexKasema <lexxmulwa@gmail.com>
2024-05-21 15:12:01 +0300

    Created a featured room component that shows contents of a featured room. Created sanityQueries file that will handle all the query to sanity studio. Created apis file in libs that fetches the data from sanity.

---
+++ src/libs/apis.ts
@@ -0,0 +1,16 @@
+//! communicates with the sanity client
+
+import { Room } from "@/models/room";
+import sanityClient from "./sanity";
+import * as queries from './sanityQueries';
+
+export async function getFeaturedRoom() {
+    const result = await sanityClient.fetch<Room>(
+      queries.getFeaturedRoomQuery,
+      {},
+      { cache: 'no-cache' }, //! this function gets called on every request
+    //!   { next: {revalidate: 1800} } // revalidate every 30 minutes
+    );
+  
+    return result;
+}
